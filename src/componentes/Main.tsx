ca66aba526e536fd4a3bc0f5f8d65674e4dee1cc
duda18vr <116743147+duda18vr@users.noreply.github.com>
2024-03-19 10:16:21 -0400

    Summary (required)

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,16 @@
+import './Main.css'
 function Main(){
-    return(
-        <>
-        <main>duda</main>
-        <h1>jsdsjadjs</h1>
-        </>
+    return( 
+        <main>
+   <div className="estudante-box">
+    <img className = "estudante-foto" src="https://github.com/duda18vr.png" alt=""/>
+    <div className="Informações">
+    <h3 className='estudante_name'>Eduarda Rodrigues</h3>
+    <p className='estudante_descrição'>Estudante do IFMS</p>
+    <a className = "link-git" href="https://github.com/" >GitHub</a>
+    </div>
+    </div>
+        </main>
     )
 }
 export default Main;
